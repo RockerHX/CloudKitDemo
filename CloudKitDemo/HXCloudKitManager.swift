ca07f40ddcc8b0ca0e07026eb26e26451b3af260
RockerHX <rockerhx@gmail.com>
2017-10-29 20:41:26 +0800

    Update[Code]: HXCloudKitManager.swift

--- CloudKitDemo/HXCloudKitManager.swift
+++ CloudKitDemo/HXCloudKitManager.swift
@@ -8,9 +8,61 @@
 
 
 import CloudKit
+import PromiseKit
 
 
 class HXCloudKitManager: NSObject {
 
     static let manager = HXCloudKitManager()
+
+    // MARK: - Public Property -
+    let cloudContainer = CKContainer.default()
+
+    private(set) var publicDatabase: CKDatabase!
+    private(set) var privateDatabase: CKDatabase!
+    private(set) var shareDatabase: CKDatabase!
+
+    // MARK: - Initialize Methods -
+    override init() {
+        super.init()
+
+        _ = checkAccountStatus()
+    }
+
+    // MARK: - Public Methods -
+    public func checkAccountStatus() -> Promise<Void> {
+        return CKContainer.default().accountStatus().then { status -> Promise<Void> in
+
+            var errorTip = ""
+            switch status {
+            case .couldNotDetermine:
+                errorTip = "accountStatus: couldNotDetermine"
+            case .available:
+                print("accountStatus: available")
+            case .restricted:
+                errorTip = "accountStatus: restricted"
+            case .noAccount:
+                errorTip = "accountStatus: noAccount"
+            }
+
+            if status == .available {
+                return Promise()
+            } else {
+                return Promise<Void> { _, reject in
+                    reject(NSError(domain: errorTip, code: -1, userInfo: nil))
+                }
+            }
+
+        }.then { Void -> Promise<Void> in
+            self.publicDatabase = self.cloudContainer.publicCloudDatabase
+            self.privateDatabase = self.cloudContainer.privateCloudDatabase
+            if #available(iOS 10.0, *) {
+                self.shareDatabase = self.cloudContainer.sharedCloudDatabase
+            } else {
+                // Fallback on earlier versions
+            }
+            return Promise()
+        }
+    }
+
 }
